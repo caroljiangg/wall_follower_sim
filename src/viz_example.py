a0b950a476e30ea8c04d7db5d377994e3bdc7ab1
caroljiangg <cjiang24@mit.edu>
2023-03-01 03:39:03 +0000

    Implement wall follower

--- src/viz_example.py
+++ src/viz_example.py
@@ -10,6 +10,7 @@ class LinePublisher:
     #the topics to publish and subscribe to
     SCAN_TOPIC = rospy.get_param("wall_follower/scan_topic")
     WALL_TOPIC = "/wall"
+    SIDE = rospy.get_param("wall_follower/side")
     
     def __init__(self):
         #a publisher for our line marker
@@ -21,8 +22,27 @@ class LinePublisher:
     def laser_callback(self, laser_scan):
         # x and y should be points on your detected wall
         # here we are just plotting a parabola as a demo
-        x = np.linspace(-2., 2., num=20)
-        y = np.square(x)
+        # x = np.linspace(-2., 2., num=20)
+        # y = np.square(x)
+        ranges = np.array(laser_scan.ranges)
+        angles = np.array([laser_scan.angle_min+laser_scan.angle_increment*i for i in range(len(ranges))])
+        if self.SIDE == -1:
+            indices = np.logical_and(angles < np.pi/6.0, angles > -1.0/2*np.pi)
+            # indices = np.logical_and(angles < np.pi/3.0, angles > -1.0/2*np.pi)
+            # indices = np.logical_and(angles < np.pi/4.0, angles > -1.0/2*np.pi)
+            x = np.cos(angles[indices]) * ranges[indices]
+            y = np.sin(angles[indices]) * ranges[indices]
+
+            # x_close = (-c*m)/(m**2+1)
+            # y_close = m*x_close + c
+        # front_ranges = ranges[33:67]
+        else:
+            indices = np.logical_and(angles < np.pi/2.0, angles > -1.0/6*np.pi)
+            # indices = np.logical_and(angles > -np.pi/2.0, angles < 1.0/3*np.pi)
+            # x = np.cos(angles[indices]) * ranges[indices]
+            # y = np.sin(angles[indices]) * ranges[indices]
+            x = np.cos(angles[60:]) * ranges[60:]
+            y = np.sin(angles[60:]) * ranges[60:]
 
         VisualizationTools.plot_line(x, y, self.line_pub, frame="/laser")
 
--- src/wall_follower.py
+++ src/wall_follower.py
@@ -21,11 +21,55 @@ class WallFollower:
         # TODO:
         # Initialize your publishers and
         # subscribers here
-
-        pass
+        self.pub = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped, queue_size=10)
+        self.prev_error = 0
+        rospy.Subscriber(self.SCAN_TOPIC, LaserScan, self.callback)
 
     # TODO:
     # Write your callback functions here.
+    def callback(self, scan):
+        ranges = np.array(scan.ranges)
+        angles = np.array([scan.angle_min+scan.angle_increment*i for i in range(len(ranges))])
+        if self.SIDE == -1:
+            indices = (angles > -np.pi/2) & (angles < np.pi/3) & (ranges < 4)
+            if np.sum(indices) == 0:
+                indices = (angles > (-np.pi/2)) & (angles < (np.pi/3)) 
+        else:
+            indices = (angles > -np.pi/3) & (angles < np.pi/2) & (ranges < 4)
+            if np.sum(indices) == 0:
+                indices = (angles > (-np.pi/3)) & (angles < (np.pi/2)) 
+        x = np.cos(angles[indices]) * ranges[indices]
+        y = np.sin(angles[indices]) * ranges[indices]
+
+        A = np.vstack([x, np.ones(len(x))]).T
+        m, c = np.linalg.lstsq(A, y)[0]
+        distance = abs(c)/np.sqrt(m**2 + 1)
+           
+        error = self.SIDE * (distance - self.DESIRED_DISTANCE)
+        
+        kp = 4
+        kd = 1.25
+
+        if np.min(ranges) < 0.2:
+            steering_angle = 0.0
+        elif error > 1:
+            steering_angle = self.SIDE*0.34
+        else:
+            error_derivative = error - self.prev_error
+            angle_error = np.arctan2(m, 1)
+            steering_angle = kp*error + kd*error_derivative + angle_error
+
+        drive_msg = AckermannDriveStamped()
+        drive_msg.header.stamp = rospy.Time.now()
+        drive_msg.header.frame_id = 'base_link'
+        drive_msg.drive.steering_angle = steering_angle
+        drive_msg.drive.steering_angle_velocity = 0
+        drive_msg.drive.speed = self.VELOCITY
+        drive_msg.drive.acceleration = 0
+        drive_msg.drive.jerk = 0
+
+        self.prev_error = error
+        self.pub.publish(drive_msg)
 
 if __name__ == "__main__":
     rospy.init_node('wall_follower')
